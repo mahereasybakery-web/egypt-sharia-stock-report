84a90d18a6e351f40b70aaa310831eb7fe6e7a16
mahereasybakery-web <maher.easybakery@gmail.com>
2026-09-17 13:38:02 +0300

    Feat: Bump Service Worker cache to v7.4 for instant Mobile & Chart updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EGX Sharia Portal - Advanced PWA Service Worker v7.3
-const CACHE_NAME = 'egx-sharia-v7.3';
+// EGX Sharia Portal - Advanced PWA Service Worker v7.4
+const CACHE_NAME = 'egx-sharia-v7.4';
 const STATIC_ASSETS = [
   './',
   './index.html',
