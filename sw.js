0b62dc71907c088eb6dee3801abcb8c85d92a150
mahereasybakery-web <maher.easybakery@gmail.com>
2026-09-17 16:19:21 +0300

    Feat(v8.3): Bump SW cache to v8.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EGX Sharia Portal - Advanced PWA Service Worker v8.1
-const CACHE_NAME = 'egx-sharia-v8.1';
+// EGX Sharia Portal - Advanced PWA Service Worker v8.2
+const CACHE_NAME = 'egx-sharia-v8.3';
 const STATIC_ASSETS = [
   './',
   './index.html',
